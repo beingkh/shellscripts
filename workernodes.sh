00e63a678484ff811d05a177923045808a1d788c
Rakesh K H <Rakeshkh@outlook.in>
2026-01-04 13:04:20 +0530

    renamed the files

--- workernodes.sh
+++ workernodes.sh
@@ -1,9 +1,19 @@
-#!/bin/bash
+sudo swapoff -a
+sudo yum install docker -y
+sudo systemctl enable --now docker.service
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
-sudo kubeadm init 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.35/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.35/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
 
-# Install Calico Network Plugin
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.31.3/manifests/calico.yaml -O
+sudo yum install -y kubelet kubeadm kubectl --setopt=disable_excludes=kubernetes
+sudo systemctl start kubelet.service
+sudo systemctl enable kubelet.service
